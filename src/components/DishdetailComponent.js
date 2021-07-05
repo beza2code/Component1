2afdba89171be40a0ae24ac0973cbd2ecef3dd86
Bezawit Gebregiorgis <bezawitgebregiorgis@gmail.com>
2021-07-05 03:02:57 -0400

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import {Card,CardImg,CardText,CardTitle,CardBody} from "reactstrap"
+import { Link } from 'react-router-dom';
 
 class Dishdetail extends Component {
 
         render() { 
             const {dish} = this.props;
-                return ( 
-            <div className="row">
-              {this.renderDish(dish)}
+            return (
+                <div className="container">
+                <div className="row">
+                    <Breadcrumb>
 
-            </div>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments} />
+                    </div>
+                </div>
+                </div>
+            
         );}
 
 
