e7d3dd9348773d1892728de98666cd4f037080c5
Bezawit Gebregiorgis <bezawitgebregiorgis@gmail.com>
2021-07-05 03:14:30 -0400

    Header and Footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
 
 function Footer(props) {
     return(
-    <div className="footer">
+        <div className="footer">
         <div className="container">
             <div className="row justify-content-center">             
                 <div className="col-4 offset-1 col-sm-2">
                     <h5>Links</h5>
                     <ul className="list-unstyled">
-                    <li><Link to='/home'>Home</Link></li>
-                        <li><Link to='/aboutus'>About Us</Link></li>
-                        <li><Link to='/menu'>Menu</Link></li>
-                        <li><Link to='/contactus'>Contact Us</Link></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Menu</a></li>
+                        <li><a href="contactus.html">Contact</a></li>
                     </ul>
                 </div>
                 <div className="col-7 col-sm-5">
@@ -24,8 +22,8 @@ function Footer(props) {
 		              HONG KONG<br />
 		              <i className="fa fa-phone fa-lg"></i>: +852 1234 5678<br />
 		              <i className="fa fa-fax fa-lg"></i>: +852 8765 4321<br />
-		              <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net"></a>
-                      confusion@food.net</a>
+		              <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
+                         confusion@food.net</a>
                     </address>
                 </div>
                 <div className="col-12 col-sm-4 align-self-center">
@@ -46,7 +44,7 @@ function Footer(props) {
             </div>
         </div>
     </div>
-    )
+    );
 }
 
-export default Footer;
+export default Footer; 
